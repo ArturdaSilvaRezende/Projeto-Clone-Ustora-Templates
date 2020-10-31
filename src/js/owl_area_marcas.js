5dfd9330fc06a8211aa90c4c4c20febe331c028f
Artur da Silva Rezende <projetosgit4@gmail.com>
2020-10-30 23:46:55 -0300

    Refatoramento do código concluido

---
+++ src/js/owl_area_marcas.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+    var owl = $('.carousel-marcas');
+
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 30,
+        nav: false,
+        dots: false,
+        slideTransition: 'linear',
+        responsive: {
+
+            //da width de 0px até 600px vai mostrar somente 1
+            0: {
+                items: 1
+            },
+
+            //da width de 600px até 900px vai mostrar somente 2
+            600: {
+                items: 2
+            },
+
+            //da width 900px até 1200px eu quero que mostre somente 3
+            900: {
+                items: 3
+            },
+
+            //chegando em 1200px irar mostrar 4 conforme foi previsto antes
+            1200: {
+                items: 4
+            }
+
+        },
+
+    });
+
+    $('.btn-prev').on('click', function () {
+        owl.trigger('prev.owl.carousel');
+    });
+
+    $('.btn-next').on('click', function () {
+        owl.trigger('next.owl.carousel');
+    });
+
+});
